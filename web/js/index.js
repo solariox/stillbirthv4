338a4ca316527a953bf8e925808d1e624da5b065
Julien <mjulien14@hotmail.fr>
2017-12-12 16:19:46 +0100

    Commit de la version symfony 3

--- web/js/index.js
+++ web/js/index.js
@@ -1,15 +1,8 @@
-var count=0;
+var flip=false;
 $( document ).ready(function() {
   setInterval(isaacAnimation, 150);
 });
 function isaacAnimation(){
-
-  if (count==0){
-    document.getElementById("isaac").src="../img/isaac1.png";
-    count++
-  }
-  else {
-    document.getElementById("isaac").src="../img/isaac2.png";
-    count=0
-  }
+  document.getElementById("isaac").src=flip?"../img/isaac1.png":"../img/isaac2.png";
+  flip=!flip
 }
